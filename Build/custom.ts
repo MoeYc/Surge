778bbac0d7f4e94112fbb64cca5472ae02d97f07
fz6m <59400654+fz6m@users.noreply.github.com>
2026-06-09 21:37:20 +0800

    feat: add twitter list

--- Build/custom.ts
+++ Build/custom.ts
@@ -13,19 +13,24 @@ export function writeCustomFile(filename: string, content: NodeJS.TypedArray | s
 export const buildCustom = task(require.main === module, __filename)(async () => {
   await mkdirp(CUSTOM_OUTPUT_DIR);
 
-  await writeCustomFile('apple_push.conf', `
-DOMAIN,identity.apple.com
-DOMAIN-SUFFIX,akadns.net
-DOMAIN-SUFFIX,push.apple.com
-DOMAIN-KEYWORD,apple.com.edgekey.net
-IP-CIDR,17.188.128.0/18,no-resolve
-IP-CIDR,17.188.20.0/23,no-resolve
-IP-CIDR,17.249.0.0/16,no-resolve
-IP-CIDR,17.252.0.0/16,no-resolve
-IP-CIDR,17.57.144.0/22,no-resolve
-IP-CIDR6,2403:300:a42::/48,no-resolve
-IP-CIDR6,2403:300:a51::/48,no-resolve
-IP-CIDR6,2620:149:a44::/48,no-resolve
-IP-CIDR6,2a01:b740:a42::/48,no-resolve
+  await writeCustomFile('twitter_non_ip.conf', `
+# >> Twitter
+DOMAIN-SUFFIX,x.com
+DOMAIN-SUFFIX,t.co
+DOMAIN-SUFFIX,twimg.co
+DOMAIN-SUFFIX,twimg.com
+DOMAIN-SUFFIX,twitpic.com
+DOMAIN-SUFFIX,twitter.com
+DOMAIN-SUFFIX,twitter.jp
+DOMAIN-SUFFIX,vine.co
+DOMAIN-SUFFIX,periscope.tv
+DOMAIN-SUFFIX,pscp.tv
+# fucktwitter
+DOMAIN-SUFFIX,vxtwitter.com
+DOMAIN-SUFFIX,fxtwitter.com
+DOMAIN-SUFFIX,fixupx.com
+DOMAIN-SUFFIX,fixvx.com
+DOMAIN-SUFFIX,twittpr.com
+DOMAIN-SUFFIX,nitter.net
 `.trim());
 });
